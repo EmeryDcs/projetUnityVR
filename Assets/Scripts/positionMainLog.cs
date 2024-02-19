97f0f8a4bd30320d6fc0bf0211ae8b36df438bca
Emery <119424953+EmeryDcs@users.noreply.github.com>
2024-02-19 23:11:48 +0100

    initial commit
    - project starter files

---
+++ Assets/Scripts/positionMainLog.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class positionMainLog : MonoBehaviour
+{
+    // public GameObject main;
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     main = GameObject.Find("Left Hand Tracking");
+    //     Debug.Log("Main Initialisée");
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     positionLog(main);
+    // }
+
+    public void positionLog()
+    {
+        Debug.Log("main.transform.position.x");
+    }
+}
