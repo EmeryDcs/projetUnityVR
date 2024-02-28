c76a6d6c5a0968ef65f4c4b1b16a8a327f49219f
Emery <119424953+EmeryDcs@users.noreply.github.com>
2024-02-28 23:41:48 +0100

    UPD_dessin possible avec main lorsque signe

--- Assets/Scripts/positionMainLog.cs
+++ Assets/Scripts/positionMainLog.cs
@@ -4,23 +4,67 @@ using UnityEngine;
 
 public class positionMainLog : MonoBehaviour
 {
-    // public GameObject main;
+    public bool dessin = false;
+    public GameObject mainDroite;
 
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-    //     main = GameObject.Find("Left Hand Tracking");
-    //     Debug.Log("Main Initialisée");
-    // }
+    public Material materialDessin;
+    private float largeur = 0.01f;
 
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     positionLog(main);
-    // }
+    private LineRenderer dessinEnCours;
+    private int index = 0;
+    private int i = 0;
 
-    public void positionLog()
+    void Update()
     {
-        Debug.Log("main.transform.position.x");
+        if (dessin)
+        {
+            // positionLog();
+            dessiner();
+        } 
+        else if (dessinEnCours != null)
+        {
+            i = 0;
+            dessinEnCours = null;
+        }
+    }
+
+    private void dessiner()
+    {
+        Debug.Log(i++);
+        if (dessinEnCours == null)
+        {
+            index = 0;
+            dessinEnCours = new GameObject().AddComponent<LineRenderer>();
+            dessinEnCours.material = materialDessin;
+            dessinEnCours.startColor = dessinEnCours.endColor = Color.red;
+            dessinEnCours.startWidth = dessinEnCours.endWidth = largeur;
+            dessinEnCours.positionCount = 1;
+            dessinEnCours.SetPosition(0, mainDroite.transform.position);
+        } 
+        else
+        {
+            var positionActuelle = dessinEnCours.GetPosition(index);
+            if (Vector3.Distance(positionActuelle, mainDroite.transform.position) > 0.001f)
+            {
+                index++;
+                dessinEnCours.positionCount = index + 1;
+                dessinEnCours.SetPosition(index, mainDroite.transform.position);
+            }
+        }
+    }
+
+    private void positionLog()
+    {
+        Debug.Log("Position : " + mainDroite.transform.position);
+    }
+
+    public void printIsFalse()
+    {
+        dessin = false;
+    }
+
+    public void printIsTrue()
+    {
+        dessin = true;
     }
 }
