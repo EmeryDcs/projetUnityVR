6bd735ab9afcc7d24f6e750b27e8ddc2806ecfec
Emery <119424953+EmeryDcs@users.noreply.github.com>
2024-02-29 07:08:09 +0100

    UPD_Reconnaissance figure (bug sur figure trait)

--- Assets/Scripts/positionMainLog.cs
+++ Assets/Scripts/positionMainLog.cs
@@ -9,15 +9,18 @@ public class positionMainLog : MonoBehaviour
     public GameObject mainDroite;
     public LineRenderer sort2d;
     public Material materialDessin;
+    public LineRenderer [] figures;
 
+    // private Vector2 [] points;
+    // private Vector2 [][] figuresVector2;
     private float largeur = 0.01f;
-
     private GameObject tipMainDroite;
     private LineRenderer dessinEnCours;
     private int index = 0;
     private int timer = 0;
 
     void Start(){
+        //Je récupère le bout du doigt pour plus d'immersion
         tipMainDroite = mainDroite.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetChild(0).gameObject;
     }
 
@@ -33,7 +36,7 @@ public class positionMainLog : MonoBehaviour
             if (timer > 29) //Ce if me permet d'éviter de supprimer le dessin trop tôt si jamais la reconnaissance n'est pas fluide.
             {
                 timer = 0;
-                dessinVers2d();
+                dessinComparaison();
                 dessinEnCours = null;
             } else {
                 dessiner();
@@ -75,29 +78,115 @@ public class positionMainLog : MonoBehaviour
         dessin = true;
     }
 
-    public void dessinVers2d(){
+    public void dessinComparaison(){
+        sort2d.positionCount = dessinEnCours.positionCount;
+
+        Vector2 difference = -conversion3dvers2d(dessinEnCours.GetPosition(0));
+
+        for (int i = 0; i < dessinEnCours.positionCount; i++)
+        {
+            Vector2 point2d = conversion3dvers2d(dessinEnCours.GetPosition(i));
+            sort2d.SetPosition(i, (point2d+difference));
+
+            Destroy(dessinEnCours.gameObject);
+        }
+
+        sort2d = reducTaille(sort2d);
+        estUneFigure(sort2d);
+    }
+
+    public LineRenderer reducTaille(LineRenderer figure)
+    {
+        float tailleMaxVecteur = 0;
+        for (int i=0; i<figure.positionCount; i++)
+        {
+            for (int j=0; j<figure.positionCount; j++)
+            {
+                float tailleVecteur = (figure.GetPosition(i) - figure.GetPosition(j)).magnitude;
+                if (tailleVecteur > tailleMaxVecteur)
+                {
+                    tailleMaxVecteur = tailleVecteur;
+                }
+            }
+        }
+
+        for (int i=0; i<figure.positionCount; i++)
+        {
+            figure.SetPosition(i, figure.GetPosition(i)/(tailleMaxVecteur/2));
+        }
+
+        return figure;
+    }
+
+    public void estUneFigure(LineRenderer sortCast)
+    {
+        float comparaison = 10;
+        float tmpComparaison = 10000;
+        int indiceFigure = 0;
+
+        for (int i = 0; i < figures.Length; i++)
+        {
+            if (sortCast.positionCount < figures[i].positionCount)
+            {
+                comparaison = comparerFigures(sortCast, figures[i]);
+            } 
+            else if (sortCast.positionCount > figures[i].positionCount) 
+            {
+
+                comparaison = comparerFigures(figures[i], sortCast);
+            } else {
+                comparaison = comparerFigures(sortCast, figures[i]);
+            }
+            if (comparaison<tmpComparaison)
+            {
+                tmpComparaison = comparaison;
+                indiceFigure = i;
+            }
+        }
+
+        if (tmpComparaison < 0.2){
+            Debug.Log("C'est la figure : "+figures[indiceFigure].gameObject.name);
+            Debug.Log("La comparaison est de : "+tmpComparaison);
+        } else {
+            Debug.Log("Ce n'est pas une figure");
+            Debug.Log("La comparaison est de : "+tmpComparaison);
+        }
+    }
+
+    public float comparerFigures(LineRenderer petiteFigure, LineRenderer grandeFigure){
+        int nombrePointsPetiteFigure = petiteFigure.positionCount;
+        int nombrePointsGrandeFigure = grandeFigure.positionCount;
+        float margeErreur = 0;
+        float distance = 0;
+        float distanceTampon = 100;
+        //Dans ces boucles, on compare chaque point de la petite figure à chaque point de la grande figure, afin de savoir si la petite figure est bien une partie de la grande figure.
+        for (int i = 0; i < nombrePointsPetiteFigure; i++)
+        {
+            distance = 0;
+            distanceTampon = 100;
+            for (int j = 0; j < nombrePointsGrandeFigure ; j++)
+            {
+                distance = (petiteFigure.GetPosition(i) - grandeFigure.GetPosition(j)).magnitude;
+                if (distance < distanceTampon){
+                    distanceTampon = distance;
+                }
+            }
+            margeErreur += distanceTampon;
+        }
+        return margeErreur/nombrePointsPetiteFigure;
+    }
+
+    public Vector2 conversion3dvers2d(Vector3 vecteur){
         Matrix4x4 matriceProjection = camera.projectionMatrix;
         Matrix4x4 matriceVue = camera.worldToCameraMatrix;
         Matrix4x4 matriceProjectionCombine = matriceProjection * matriceVue;
 
-        sort2d.positionCount = dessinEnCours.positionCount;
-
-        Vector2 difference = -(new Vector2(
-            ((matriceProjectionCombine.MultiplyPoint3x4(dessinEnCours.GetPosition(0)).x / matriceProjectionCombine.MultiplyPoint3x4(dessinEnCours.GetPosition(0)).z) + 1) / 2 * Screen.width,
-            ((matriceProjectionCombine.MultiplyPoint3x4(dessinEnCours.GetPosition(0)).y / matriceProjectionCombine.MultiplyPoint3x4(dessinEnCours.GetPosition(0)).z) +1) / 2 * Screen.height
-        ));
-
-        for (int i = 0; i < dessinEnCours.positionCount; i++)
-        {
-            Vector3 point = dessinEnCours.GetPosition(i);
-            Vector3 pointProjete = matriceProjectionCombine.MultiplyPoint3x4(point);
-            Vector2 point2d = new Vector2(
-                ((pointProjete.x / pointProjete.z) + 1) / 2 * Screen.width,
-                ((pointProjete.y / pointProjete.z) + 1) / 2 * Screen.height
-            );
-            sort2d.SetPosition(i, (point2d+difference)/100);
-            Debug.Log(point2d);
-            Destroy(dessinEnCours.gameObject);
-        }
+        Vector3 point = vecteur;
+        Vector3 pointProjete = matriceProjectionCombine.MultiplyPoint3x4(point);
+        Vector2 point2d = new Vector2(
+            ((pointProjete.x / pointProjete.z) + 1) / 2 * Screen.width,
+            ((pointProjete.y / pointProjete.z) + 1) / 2 * Screen.height
+        );
+        return point2d;
     }
 }
